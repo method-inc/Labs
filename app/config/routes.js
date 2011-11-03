aade37af7db66f177880acf3ab61ec739988af53
Jim Snodgrass <jim@skookum.com>
2011-11-03 15:05:39 -0400

    add extra demos route to match old route that may be in reddit, favorites, etc

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,9 +1,11 @@
-var home = require('../controllers/home')
-  , labs = require('../controllers/labs');
+var home = require('../controllers/home'),
+    labs = require('../controllers/labs');
 
 exports = module.exports = function(server) {
 
+  server.all('/demos/:section/:id', labs.section );
+  server.all('/demos/:section', labs.index);
   server.all('/:section/:id', labs.section );
   server.all('/:section', labs.index);
   server.all('/', home.index);
-}
+};
