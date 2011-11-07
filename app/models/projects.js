35604443e6a5704c4a89be1f9a3d66f0c3b2ce5b
David Becher <david@skookum.com>
2011-11-07 17:56:38 -0500

    Add knockout viewmodels for filtering projects
    Signed-off-by: David Becher <david@skookum.com>

--- app/models/projects.js
+++ app/models/projects.js
@@ -14,52 +14,56 @@ Projects.prototype.init = function(server) {
 
 Projects.prototype.find_all = function(callback) {
   
-  projects = {
-    featured: [
+  projects = [
       { name: 'Clickdummy',
+        featured: true,
         description: 'Mockups That Click!  Clickdummy allows you to upload images for your website design and link them together.  You can also add comments and reply to other comments.  You can also share your projects with friends and coworkers to get feedback or let them help build it out.',
         link: 'https://clickdummy.com', 
         screenshot:'clickdummy.jpg', 
-        tags:['html5', 'css3', 'knockoutjs']}
-    ],
-
-    demos: [
+        tags:['html5', 'css3', 'knockoutjs']},
       { name: 'BarcampCLT Physics',
+        featured: false,
         description: 'This is the presentation that Hunter Loftis gave at BarcampCLT in fall of 2010.  It goes over the basic concepts of using physics in your apps, especially for games. Each page includes the code needed for that demo.',
         link: '/demos/barcampclt_physics', 
         screenshot:'physics.jpg', 
         tags:['html5', 'physics', 'canvas', 'code examples']},
       { name: 'Canvas Performace Test',
+        featured: false,
         description: 'There are lots of different ways to do lots of different things with canvas.  This simple demo allows you to dynamically switch between different techniques and settings and see the affect on performance in your browser.',
         link: '/demos/canvastest', 
         screenshot:'canvas_test.jpg', 
         tags:['html5', 'physics', 'canvas']},
       { name: 'Skookball Prototype',
+        featured: false,
         description: 'This demo shows off canvas in a game setting with fast, responsive action and controls.  Play it on your iPad and experience motion controls as well!', 
         link: '/demos/skookball', 
         screenshot:'Skookball_proto.jpg', 
         tags:['html5', 'physics', 'game', 'canvas']},
       { name: 'KnockoutJS ToDo List',
+        featured: false,
         description: 'This demo was made for a presentation on Knockout.js and the benefits of using declaritive UI binding for front-end interfaces. It also includes local storage for persistance.',
         link: '/demos/knockout_todos', 
         screenshot:'knockout_todos.jpg',  
         tags:['html5', 'canvas', 'knockoutjs']},
       { name: 'Dueling Pianos',
+        featured: false,
         description: 'Dueling Pianos was our entry for node knockout.  It was built in a weekend by three guys.  It uses Youtube API\'s for the music, CSS3 transitions and transforms for the playing board, and node.js/web sockets for multiplayer.  Scoring is done on both the client and server using shared modules. There are a few lingering bugs, but it is fun and will continue to get better. ', 
         link: this.host+':3100', 
         screenshot:'DuelingPianos.jpg', 
         tags:['html5', 'audio', 'game', 'css3']},
       { name: 'HTML5 Fish Aquarium',
+        featured: false,
         description: 'This was built for the javascript 10k contest in 2010.  The original version, when minified, was less then 10k in size (it\'s expanded now so you can read the code).  Everything is created in code, there are no images.', 
         link: this.host+':3200', 
         screenshot:'FishAquarium.jpg', 
         tags:['html5', 'ccss3', 'canvas']},
       { name: 'Tribes 2D : Operation MAX', 
+        featured: false,
         description: 'This is a demonstration of canvas graphics and multiplayer web socket functionality.  Every viewer gets a character to move around the board and can see any other viewers on the page.',
         link: this.host+':3300', 
         screenshot:'Tribes.jpg', 
         tags:['html5', 'physics', 'game', 'canvas']}
-  ]};
+  ];
 
   callback(null, projects);
 };
--- app/public/js/labs.js
+++ app/public/js/labs.js
@@ -1,2 +1,93 @@
 $('#background-container img').plaxify();
 $.plax.enable();
+
+// Projects browser viewmodel
+function ProjectsVM() {
+  // Projects array
+  this.projects = ko.observableArray([]);
+  
+  this.featured_projects = ko.dependentObservable(function() {
+    return _.filter(this.projects(), function(p) { return p.featured(); });
+  }, this);
+  this.nonfeatured_projects = ko.dependentObservable(function() {
+    return _.select(this.projects(), function(p) { return !p.featured(); });
+  }, this);
+  
+  // Get the tags from each project and merge them into one array
+  this.tags = ko.dependentObservable(function() {
+    var self = this;
+    return _(this.projects()).chain().
+      map(function(p) { return p.tags(); }).
+      flatten().
+      uniq(false, function(t) { return t.name(); }).
+      value();
+  }, this);
+  
+  this.selectedTags = ko.observableArray([]);
+  
+  this.viewingAll = ko.dependentObservable(function() {
+    return this.selectedTags().length == 0;
+  }, this);
+}
+
+ProjectsVM.prototype = {
+  load: function(json) {
+    var self = this;
+    this.projects(_.map(json, function(p) {
+      return new ProjectModel(p, self);
+    }));
+  },
+  
+  toggleTag: function(tag) {
+    if(_.any(this.selectedTags(), function(t) {return tag.name() === t.name();})) {
+      this.selectedTags.remove(function(t) {
+        return t.name() === tag.name();
+      });
+    }
+    else {
+      this.selectedTags.push(_.detect( this.tags(), function(t) { return t.name() === tag.name(); } ));
+    }
+  },
+  clearTags: function() {
+    this.selectedTags([]);
+  }
+}
+
+
+function ProjectModel(data, parent) {
+  this.parent = parent;
+  
+  this.name = ko.observable(data.name);
+  this.featured = ko.observable(data.featured);
+  this.description = ko.observable(data.description);
+  this.link = ko.observable(data.link);
+  this.screenshot = ko.observable('/images/screenshots/' + data.screenshot);
+  this.tags = ko.observableArray(_.map(data.tags, function(t) {
+    return new TagModel(t, parent);
+  }));
+  
+  this.visible = ko.dependentObservable(function() {
+    var self = this;
+    return _.all(this.parent.selectedTags(), function(tag) {
+      return self.hasTag(tag);
+    });
+  }, this);
+  
+  var self = this;
+  this.hasTag = function(tag) {
+    return _.any(self.tags(), function( t ) {
+    	return t.name() === tag.name();
+    });
+  }
+}
+
+function TagModel(name, parent) {
+  this.name = ko.observable(name);
+  this.selected = ko.dependentObservable(function() {
+    return _.indexOf(parent.selectedTags(), this.name()) > -1;
+  }, this);
+  var self = this;
+  this.toggle = function() {
+    parent.toggleTag(self);
+  }
+}
