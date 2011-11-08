5d17d1e2e7cc10d22962c82a4d165f338b38488a
David Becher <david@skookum.com>
2011-11-08 12:12:04 -0500

    Add transitions to product filtering
    Signed-off-by: David Becher <david@skookum.com>

--- app/public/js/labs.js
+++ app/public/js/labs.js
@@ -21,19 +21,19 @@ function ProjectsVM() {
       flatten().
       uniq(false, function(t) { return t.name(); }).
       value();
-  }, this);
+  }, this).extend({ throttle: 100 });
   
   this.selectedTags = ko.observableArray([]);
   
   this.viewingAll = ko.dependentObservable(function() {
     return this.selectedTags().length == 0;
-  }, this);
+  }, this).extend({ throttle: 100 });
   
   this.projectsEmpty = ko.dependentObservable(function() {
     return _.all(this.projects(), function(p) {
       return !p.visible();
     });
-  }, this);
+  }, this).extend({ throttle: 100 });
 }
 
 ProjectsVM.prototype = {
@@ -99,3 +99,94 @@ function TagModel(name, parent) {
     parent.toggleTag(self);
   }
 }
+
+
+// KO Binding to "morph" one element into another, fading it and changing its height to match
+// the new element. Use the "show" option to specify if the element should be shown. 
+// Pass a selector as the "connected" option to specify which element it should fade in to. 
+// This function will try to intelligently figure out which of the connected elements is 
+// the old element.
+ko.bindingHandlers.morph = {
+  _initializing: true,
+  init: function (element, valueAccessor) {
+    // don't do the animation when the bindings are being set up
+    window.setTimeout(function() {ko.bindingHandlers.morph._initializing = false;}, 500);
+    // Initially set the element to be instantly visible/hidden depending on the value
+    var value = valueAccessor();
+    $(element).toggle(ko.utils.unwrapObservable(value.show) == true); // Use "unwrapObservable" so we can handle values that may or may not be observable
+  },
+  update: function (element, valueAccessor) {
+    // don't do the animation on page load
+    if(!ko.bindingHandlers.morph._initializing) {
+      // Get the connected element, in the case where there is more than one possibility
+      function getConnected(selector) {
+        if(selector.search(/[a-zA-Z]/) == 0) selector = "#" + selector;
+        var connected = $(selector);
+        if( connected.length == 1 )
+          return connected;
+        else if(connected.length > 1) {
+          var found = null;
+          // find the element that's not hidden (and not this element)
+          connected.each(function() {
+            if($(this).css('display') == '' || $(this).css('display') == 'block' && this != element) {
+              found = $(this);
+              return false;
+            }
+          });
+          if(found) return found;
+          else return $('<div></div>');
+        }
+        else return $('<div></div>');
+      }
+      
+      var value = valueAccessor(),
+          e = $(element);
+      // unwrap all values
+      _.each(value, function(v, key) {
+        value[key] = ko.utils.unwrapObservable(v);
+      });
+      // set some defaults for the animation
+      value = _.defaults(value, { 
+        duration: 250,
+        connected: '',
+        easing: 'linear'
+      });
+      
+      // Show the new element
+      if( value.show == true ) {
+        if($(element).css('display') == 'none') {
+          e.show();
+          var connected = getConnected(value.connected),
+              newHeight = e.height(),
+              wrap = e.wrap('<div>');
+          e.data('originalHeight', newHeight);
+          wrap.css({ 'overflow-y': 'hidden', height: connected.height() + 'px', opacity: 0 });
+          wrap.animate({ height: newHeight + 'px', opacity: 1 }, value.duration, value.easing, function() {
+            wrap.css({ 'overflow-y': '', height: '', width: '', opacity: '' });
+            e.unwrap();
+            e.data('originalHeight', null);
+          });
+        }
+      }
+      // Hide the old element if it's shown
+      else if($(element).css('display') == '' || $(element).css('display') == 'block') {
+        var self = this,
+            wrap = e.wrap('<div>');
+        // Sometimes, this is called before the new element has a chance to appear, causing
+        // the scroll position to get messed up while this element is positioned absolutely
+        // and the new element is still hidden.
+        var oldScrollTop = $(window).scrollTop();
+        _.delay(function() { $(window).scrollTop(oldScrollTop); }, 10);
+        wrap.css({ 'overflow-y': 'hidden', position: 'absolute', top: 0, left: 0, opacity: 1, width: e.width() });
+        var connected = getConnected.call(self, value.connected),
+            newHeight = connected.data('originalHeight') || connected.height();
+        e.show();
+        wrap.animate({ height: newHeight + 'px', opacity: 0 }, value.duration-50, value.easing, function() {
+          wrap.css({ 'overflow-y': '', height: '', width: '', opacity: '', position: '', top: '', left: '' });
+          wrap.hide();
+          e.hide().unwrap();
+        });
+      }
+    }
+  }
+};
