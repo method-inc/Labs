9940109b230de1c64a5cbf04d634e16b4c327c54
Mark Flowers <mark.s.flowers@gmail.com>
2015-10-27 09:27:24 -0400

    Remove everything from git for this branch.

--- app/controllers/home.js
+++
@@ -1,9 +0,0 @@
-exports = module.exports = {
-
-  index: function(req, res, next) {
-    require('../models/projects').find_all(function(err, projects){ 
-      res.render('home/index', { projects: projects, page_title: 'Labs' });  
-    });
-  }
-  
-};
